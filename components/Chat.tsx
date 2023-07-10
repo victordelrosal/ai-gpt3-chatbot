91aa8c13d70710d61a58c2ab1f168330ad93c8f7
Victor del Rosal <45711996+victordelrosal@users.noreply.github.com>
2023-07-10 14:04:18 +0100

    Update Chat.tsx

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -9,7 +9,7 @@ const COOKIE_NAME = 'nextjs-example-ai-chat-gpt3'
 export const initialMessages: ChatGPTMessage[] = [
   {
     role: 'assistant',
-    content: 'Hi! I am a friendly AI assistant. Ask me anything!',
+    content: 'Hi! I am VictorGPT a friendly AI assistant. Ask me anything!',
   },
 ]
 
